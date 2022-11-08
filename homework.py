f4d863eac490cc0dfc0e594d94c214558faff3ac
Kovaleva Anastasia <anastasia-yurjevna@yandex.ru>
2022-11-09 02:48:51 +0700

    1

--- homework.py
+++ homework.py
@@ -1,15 +1,25 @@
-...
+import logging
+import os
+import time
+ 
+import requests
+from telegram import Bot
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, Updater
+
+from dotenv import load_dotenv 
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+HEADERS = {'Authorization': f'OAuth PRACTICUM_TOKEN'}
 
 
 HOMEWORK_STATUSES = {
@@ -20,64 +30,82 @@ HOMEWORK_STATUSES = {
 
 
 def send_message(bot, message):
-    ...
+    return bot.send_message(TELEGRAM_CHAT_ID, message)
 
 
 def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
+    response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    return response.json()
 
-    ...
 
 
-def check_response(response):
+#def check_response(response):#
 
-    ...
+    #...
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
+    homework_name = homework.get('homework_name')
+    if homework_name is None:
+        logging.error('Не удалось получить название работы')
+    homework_status = homework.get('status')
+    if homework_status is None:
+        logging.error('Не удалось получить статус работы')
 
-    ...
 
-    verdict = ...
+    #...
+    else:
+        verdict = HOMEWORK_STATUSES.get(homework_status)
 
-    ...
+    #...
 
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+        return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
-    ...
+    ENV_VARS = [PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]
+    if not all(ENV_VARS):
+        print('Отсутствует переменная')
+    else:
+        return True
 
 
 def main():
-    """Основная логика работы бота."""
+    #"""Основная логика работы бота."""
 
-    ...
+    #...
+    
 
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    bot = Bot(token='TELEGRAM_TOKEN')
     current_timestamp = int(time.time())
-
-    ...
+    STATUS = ''
+    #...
 
     while True:
-        try:
-            response = ...
+        #try:
+            response = get_api_answer(current_timestamp)
 
-            ...
-
-            current_timestamp = ...
+            #...
+            message = parse_status((response))
+            
+            if message != STATUS:
+                send_message(bot, message)
+                STATUS = message   
             time.sleep(RETRY_TIME)
+            
 
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-            time.sleep(RETRY_TIME)
-        else:
-            ...
-
+        #except Exception as error:
+            #message = f'Сбой в работе программы: {error}'
+            #...
+            #time.sleep(RETRY_TIME)
+        #else:
+            #...
 
 if __name__ == '__main__':
     main()
+
+
+
+  
