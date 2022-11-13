b04450abd0988ad21f356a25ecffc86710012bee
Kovaleva Anastasia <anastasia-yurjevna@yandex.ru>
2022-11-13 22:39:49 +0700

    8

--- homework.py
+++ homework.py
@@ -28,56 +28,67 @@ HOMEWORK_STATUSES = {
 
 class Error(Exception):
     """Базовый класс исключений."""
+
     pass
 
 
 class NoKeyInAPIResponseError(Error):
     """Отсутствует ключ "status" в ответе API."""
+
     pass
 
 
 class UnknownHomeWorkStatusError(Error):
     """Неизвестный статус домашней работы."""
+
     pass
 
 
 class APIRequestError(Error):
     """Ошибка при запросе к API."""
+
     pass
 
 
 class StatusError(Error):
     """Ошибка в статусе."""
+
     pass
 
 
 class StatusError(Error):
     """Ошибка в статусе."""
+
     pass
 
 
 class AnswerJsonError(ValueError):
     """Ошибка парсинга ответа из формата json."""
+
     pass
 
 
 class HomeWorkDictionaryError(KeyError):
     """Ошибка словаря по ключу homeworks."""
+
     pass
 
 
 class HomeWorkKeyError(KeyError):
     """Отсутствует ключ "homework_name" в ответе API."""
+
     pass
 
 
 class APIResponseDifferentDictionary(TypeError):
     """Ответ API отличен от словаря."""
+
     pass
 
 
 class HomeworkListEmpty(IndexError):
     """Список домашних работ пуст."""
+
     pass
 
 
